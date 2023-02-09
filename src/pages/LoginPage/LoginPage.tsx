a0b41c7fbda994398e28e8961b8a2bcc84342910
Valeriya <lerarozhentseva@gmail.com>
2023-02-09 20:22:39 +0300

    fix: rewrite auth form without using react-hook-form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,5 @@
 import React, { FC } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
-import { authService } from '../../graphql/auth/authService';
-import { ILoginResult } from '../../graphql/auth/IAuthResult';
 import { LoginQuery } from '../../graphql/auth/query';
-import { IInput } from '../../graphql/auth/IInput';
 import Form from '../Form';
 
 export const LoginFormType = {
@@ -14,18 +9,7 @@ export const LoginFormType = {
 };
 
 const LoginPage: FC = () => {
-  const [login, { loading }] = useLazyQuery<ILoginResult>(LoginQuery);
-  const navigate = useNavigate();
-
-  const onSubmit = async (input: IInput) => {
-    const { data } = await login({ variables: input });
-    if (data) {
-      authService.addUserToStorage(data.login.user, data.login.access_token);
-      navigate('/employees');
-    }
-  };
-
-  return <Form formType={LoginFormType} loading={loading} onSubmit={onSubmit} />;
+  return <Form formType={LoginFormType} queryAuth={LoginQuery} />;
 };
 
 export default LoginPage;
