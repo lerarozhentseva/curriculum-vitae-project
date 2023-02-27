f141861eea1fed666e96e2f4133dbe376c4c360b
Daniel B <[omitted]>
2023-02-27 18:00:43 +0300

    feat!: EmployeesPage now uses EmployeesTable component

--- src/pages/EmployeesPage/EmployeesPage.tsx
+++ src/pages/EmployeesPage/EmployeesPage.tsx
@@ -1,13 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GetUsersQuery } from '@graphql/users/GetUsersQuery';
+import { IUser } from '@graphql/interfaces/IUser';
 import Header from '@components/Header/Header';
 import Breadcrumb from '@components/Breadcrumbs/Breadcrumbs';
+import { EmployeesTable } from './components/EmployeesTable';
 
 const EmployeesPage = () => {
   const currentPath = useLocation().pathname;
+  const { data, loading, error } = useQuery<{ users: IUser[] }>(GetUsersQuery);
   return (
     <>
       <Header />
       <Breadcrumb currentPath={currentPath} />
+      <EmployeesTable users={data?.users ?? []} isLoading={loading} />
     </>
   );
 };
