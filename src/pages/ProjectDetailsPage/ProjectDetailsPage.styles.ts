c22d1a53c8752e86a94b8c4b7b53e241478a9b3c
Valeriya <lerarozhentseva@gmail.com>
2023-03-10 00:42:41 +0300

    refactor: delete unused styles

--- src/pages/ProjectDetailsPage/ProjectDetailsPage.styles.ts
+++ src/pages/ProjectDetailsPage/ProjectDetailsPage.styles.ts
@@ -1,5 +1,4 @@
 import { Paper, styled } from '@mui/material';
-import { NotificationAlert } from '@components/NotificationAlert';
 
 export const StyledPaper = styled(Paper)({
   display: 'flex',
@@ -7,11 +6,4 @@ export const StyledPaper = styled(Paper)({
   alignItems: 'center',
   padding: '30px 40px',
   margin: '20px 40px'
-});
-
-export const StyledNotificationAlert = styled(NotificationAlert)({
-  position: 'fixed',
-  bottom: '10px',
-  left: '10px',
-  width: '300px'
 });
