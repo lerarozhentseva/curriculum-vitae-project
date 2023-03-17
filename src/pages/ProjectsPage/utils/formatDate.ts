36b3aab79eb94fbd1037e41fcca95868fa90c73c
Daniel B <[omitted]>
2023-03-17 11:42:16 +0300

    fix: fixed incorrect date formatting

--- src/pages/ProjectsPage/utils/formatDate.ts
+++ src/pages/ProjectsPage/utils/formatDate.ts
@@ -1,6 +1,8 @@
 const formatDate = (date?: Date) => {
   const toFormat = date ?? new Date();
-  return `${toFormat.getFullYear()}-${toFormat.getMonth()}-${toFormat.getDate()}`;
+  return `${toFormat.getFullYear()}-${('' + (toFormat.getMonth() + 1)).padStart(2, '0')}-${(
+    '' + toFormat.getDate()
+  ).padStart(2, '0')}`;
 };
 
 export default formatDate;
