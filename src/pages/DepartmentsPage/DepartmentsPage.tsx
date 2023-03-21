0f1caa62f8ade7efa130787e5019c7f861d0e610
Daniel B <[omitted]>
2023-03-21 14:09:06 +0300

    fix: fixed department creation form showing up without admin privileges

--- src/pages/DepartmentsPage/DepartmentsPage.tsx
+++ src/pages/DepartmentsPage/DepartmentsPage.tsx
@@ -1,10 +1,12 @@
-import { useQuery } from '@apollo/client';
+import { useQuery, useReactiveVar } from '@apollo/client';
 import { Breadcrumb } from '@components/Breadcrumbs';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Toast } from '@components/Toast';
 import { GetDepartmentsQuery } from '@graphql/departments';
 import { IDepartment } from '@graphql/interfaces';
+import { authService } from '@graphql/auth/authService';
+import isAdmin from '@graphql/user/isAdmin';
 import { useCompoundError, useFilter } from '@hooks/index';
 import { DepartmentsTable } from './components/DepartmentsTable';
 import { CreateDepartmentForm } from './components/CreateDepartmentForm';
@@ -18,12 +20,14 @@ const DepartmentsPage = () => {
 
   const { error, clearError } = useCompoundError(nativeError);
 
+  const user = useReactiveVar(authService.user$);
+
   return (
     <>
       <Toast severity="error" message={error} onClose={clearError} />
       <Header />
       <Breadcrumb />
-      <CreateDepartmentForm />
+      {isAdmin(user) && <CreateDepartmentForm />}
       <Filter query={query} onChange={onQueryChange} />
       <DepartmentsTable departments={filteredDepartments} isLoading={loading} />
     </>
