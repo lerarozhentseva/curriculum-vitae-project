05cc3fa8666a6c58becc95f24d8e3e2797abb07f
Daniel B <[omitted]>
2023-03-21 13:52:10 +0300

    refactor: added CreateButton export path to the index file

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -4,3 +4,4 @@ export { default as CreateEmployeeButton } from './CreateButton';
 export { default as EmployeeCreationModalButton } from './EmployeeCreationModalButton';
 export { default as MainAuthButton } from './MainAuthButton';
 export { default as SecondaryAuthButton } from './SecondaryAuthButton';
+export { default as CreateButton } from './CreateButton';
